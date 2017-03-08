0b15be841673188fb39b5c9a85288d8c64ae624f
Wallawee <limhy.valerie@gmail.com>
2017-03-09 01:41:36 +0800

    Add files via upload

--- Class Exercise 7.py
+++ Class Exercise 7.py
@@ -1,59 +1,126 @@
 # Valerie Lim
-# Title: Class Exercise 4
-# Date: 7 March 2017
+# Date started: 6 March 2017
+# Completed: 9 March 2017 (!!!!!)
+# Title: Class Exercise 7
 
-#### SAMPLE
+# I HATED THIS SHIT
 
-class Point:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
+# Ex 4.4
+
+class Shoe:
+    def __init__(self, colour, brand):
+        self.colour = colour
+        self.brand = brand
 
     def __str__(self):
-        return '(' + str(self.x) + ', ' + str(self.y) + ')'
+        return str(self.colour)+"-coloured "+str(self.brand)
 
+    def mud(self):
+        self.colour = "brown"
+        print "Oh no, I stepped into mud!"
+        print "Now my shoes are", str(self)
 
-##### Ex 4.4
-class Cup:
-    def __init__(self, volume, colour):
-        self.volume = volume        # in ml
-        self.colour = colour
+class schoolShoe(Shoe):
+    def __init__(self, colour, highorlow, liningColour):
+        Shoe.__init__(self, "White", "Bata")
+        self.top = highorlow
+        self.lining = liningColour
 
-    def pourwater(self):
-        water = input("How much water would you like to pour in?")
-        if water > self.volume:
-            print "Oh no! The cup is overflowing!!"
-        print "The cup is now", str(float(water/self.volume *100))+"% full"
+    def __str__(self):
+        return str(self.colour)+"-coloured "+str(self.top)+" "+str(self.brand)+" shoe with "+str(self.lining)+"-coloured trimmings."
 
-    def description(self):
-        print "The cup is", str(self.volume)+"ml", "big and", self.colour, "in colour."
+    def wash(self):
+        self.colour = "white"
+        print "---------Washed!"
+        print "My shoes are back to being a", str(self)
+    
+# Test cases
+s1 = Shoe("pink", "ballet flats")
+s2 = schoolShoe("white", "low-cut", "black")        
 
-class tumbler(Cup):
-    def __init__(self, waterproof, cost):
-        Cup.__init__(self, 1000, colour)
-        self.waterproof = waterproof    # yes or no
-        self.cost = cost
-        
-class bottle(Cup):
-    def __init__(self, material, age):
-        Cup.__init__(self, 600, "transparent")
-        self.material = material
-        self.age = age      # days old cos gross mah
-
-class jug(Cup):
-    def __init__(self, owner, location):
-        self.owner = owner
-        self.location = location    # eg kept in kitchen
-
-##### Test cases
-starbucks = Cup(500, "white")
+print "My first pair of shoes are", s1
+s1.mud()
+print "My second pair of shoes are", s2
+s2.mud()
+s2.wash()
+print
+print "lol I had fun messing with this :-)"
+print 
 
 
 
-##### Ex 4.5
+#### Ex 4.5
+class Spell:
+    def __init__(self, incantation, name):
+        self.name = name
+        self.incantation = incantation
+    def __str__(self):
+        return self.name
+    def get_description(self):
+        return "No description"
+    def execute(self):
+        print self.incantation
+class Accio(Spell):
+    def __init__(self):
+        Spell.__init__(self, "Accio", "Summoning Charm")
+    def __str__(self):
+        return "This charm summons an object to the caster, potentially over a significant distance."
 
-# 1. Parent class is Spell. Child classes are the instances of spells - Accio and Confundo.
-# 2. spell = Acccio() prints nothing
-#    spell.execute() prints the self-incantation for Accio, aka "Accio"
-#    study_spell(spell) should print an error or the location of the Spell class
-#    study_spell(Confundo()) will print "Confundo"
+class Confundo(Spell):
+    def __init__(self):
+        Spell.__init__(self, "Confundo", "Confundus Charm")
+    def get_description(self):
+        return "Causes the victim to become confused and befuddled."
+
+def study_spell(spell):
+    print spell
+    
+spell = Accio()
+spell.execute()
+study_spell(spell)
+study_spell(Confundo())
+
+
+# 1.
+# Parent class: Spell
+# Chidl class: Accio, Confundo
+
+# 2.
+# Accio
+# Summoning Charm
+# Confundus Charm
+
+# 3.
+# The method from the instance of Confundo.
+# This is called (not the parent class "Spell"-'s version)
+# because the function study_spell calls the Confundo instance first
+
+# 4.
+#    def __str__(self):
+#        return "This charm summons an object to the caster, potentially over a significant distance."
+# Note: Solution has been added into code above :-)
+
+
+
+
+##### Ex 4.6
+
+class Address:
+    def __init__(self, street_name, num):
+        self.street = street_name
+        self.number = num
+    
+class CampusAddress(Address):
+    def __init__(self, office_number):
+        Address.__init__(self, "Massachusetts Ave", 77)
+        self.office_number = office_number
+
+    def __str__(self):
+        return self.street+" "+str(self.number)+" "+str(self.office_number)
+
+# Testcases
+Sarina_addr = CampusAddress("32-G904")
+print "The full address is", Sarina_addr
+print "The office number is", Sarina_addr.office_number
+print "The street name is", Sarina_addr.street
+print "The street number is", Sarina_addr.number
